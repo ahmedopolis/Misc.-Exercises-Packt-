c49c5876173a7c37b88a903fdb1e8d631d0a9ef0
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-08-05 06:19:55 -0400

    :truck: Move Files: Add practice exercises and activities

---
+++ React/2. Dealing with React Events/Exercise 2.01 Writing the Starter State for Our Code/exercise_2.01/src/App.js
@@ -0,0 +1,44 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      passwordConfirmation: "",
+      email: "",
+      errors: [],
+    };
+  }
+
+  displayForm() {
+    return (
+      <div>
+        Username: <input type="text" />
+        <br />
+        Password: <input type="text" />
+        <br />
+        Password Confirmation: <input type="text" />
+        <br />
+        Email: <input type="text" />
+        <br />
+        <br />
+        <button>Submit</button>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Create Account
+        <hr />
+        {this.displayForm()}
+      </div>
+    );
+  }
+}
+
+export default App;
