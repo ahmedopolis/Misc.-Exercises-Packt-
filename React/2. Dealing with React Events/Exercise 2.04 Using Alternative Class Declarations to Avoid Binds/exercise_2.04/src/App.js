0e77aa0eda861ecde90fbf91e40eefbc1d5e6f2c
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-08-09 23:42:36 -0400

    :truck: Move Files: Add practice exercises and activities

--- React/2. Dealing with React Events/Exercise 2.04 Using Alternative Class Declarations to Avoid Binds/exercise_2.04/src/App.js
+++ React/2. Dealing with React Events/Exercise 2.04 Using Alternative Class Declarations to Avoid Binds/exercise_2.04/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+class App extends Component {
+  state = {
+    username: "",
+    password: "",
+    passwordConfirmation: "",
+    email: "",
+    errors: [],
+  };
+  validateUsernameOnBlur = (event) => {
+    console.log("I should validate whatever is in ", event.target.value);
+    this.setState();
+  };
+  displayForm() {
+    return (
+      <div>
+        Username: <input type="text" onBlur={this.validateUsernameOnBlur} />
+        <br />
+        Password: <input type="text" />
+        <br />
+        Password Confirmation: <input type="text" />
+        <br />
+        Email: <input type="text" />
+        <br />
+        <br />
+        <button onClick={this.submitForm}>Submit</button>
+      </div>
+    );
+  }
+  submitForm(event) {
+    console.log("Submitting the form now...");
+    console.log(event);
+  }
+  render() {
+    return (
+      <div className="App">
+        Create Account
+        <hr />
+        {this.displayForm()}
+      </div>
+    );
+  }
 }
-
 export default App;
